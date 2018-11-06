2a1131de0373282b36474af1ffd28063a1002387
Klopp1989 <to_sailing@163.com>
2018-11-06 16:47:24 +0800

    update

---
+++ src/com/run/com/run/demo06/CircleString.java
@@ -0,0 +1,19 @@
+package com.run.com.run.demo06;
+
+public class CircleString {
+
+    public static boolean isCircle(String s) {
+        char[] array = s.toCharArray();
+
+        for (int i = 0; i < array.length / 2; i++) {
+            if(array[i] != array[array.length - i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isCircle("levvel"));
+    }
+}
---
+++ src/com/run/com/run/demo06/SingleList.java
@@ -0,0 +1,10 @@
+package com.run.com.run.demo06;
+
+public class SingleList {
+
+    private Node head;
+
+    public SingleList(Node head) {
+        this.head = head;
+    }
+}
