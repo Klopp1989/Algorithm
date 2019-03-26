6d686e6187e5ca05397a8f45276b66b33394438e
Klopp1989 <to_sailing@163.com>
2019-03-26 17:28:30 +0800

    update

--- src/com/chapter11/SelectSort.java
+++ src/com/chapter11/SelectSort.java
@@ -4,32 +4,33 @@ import com.util.StringUtil;
 
 /**
  * 选择排序
+ *
  * @author 杨帆
  */
 public class SelectSort {
 
-	public static void sort(int[] arr) {
-		for(int i = 0; i < arr.length; i++) {
-			// 找最小值
-			int tmp = arr[i];
-			int index = i;
-			for(int j = i; j < arr.length; j++) {
-				if(arr[j] < tmp) {
-					tmp = arr[j];
-					index = j;
-				}
-			}
-			
-			// 交换
-			int tmp2 = arr[i];
-			arr[i] = arr[index];
-			arr[index] = tmp2;
-		}
-	}
-	
-	public static void main(String[] args) {
-		int[] arr = {4, 5, 6, 1, 3, 2};
-		sort(arr);
-		StringUtil.arr2string(arr);
-	}
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            // 找最小值
+            int tmp = arr[i];
+            int index = i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j] < tmp) {
+                    tmp = arr[j];
+                    index = j;
+                }
+            }
+
+            // 交换
+            int tmp2 = arr[i];
+            arr[i] = arr[index];
+            arr[index] = tmp2;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 1, 3, 2};
+        sort(arr);
+        StringUtil.arr2string(arr);
+    }
 }
